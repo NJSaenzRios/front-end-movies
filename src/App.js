9becc13e68caf14a3826b0f0453394878266ddac
johanasaenz2021 <nancy.saenz@est.iudigital.edu.cogit config --global user.email nancy.saenz@est.iudigital.edu.co>
2021-04-09 16:48:01 -0500

    tercer commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import Table from './components/Table';
+
+function App() {
+  return (
+    <div className="container">
+
+       <NavBar />
+      
+       <Table />
+
+    </div>
+  );
+}
+
+export default App;
